5a5e6890e1f156ef70f7a4425f1ba595622b63e7
Aditi Patil <patiladiti240@gmail.com>
2021-12-13 19:46:50 +0530

    Added MinMax.py

---
+++ Array/MinMax.py
@@ -0,0 +1,9 @@
+################################################
+# Find minimum and maximum element in an array #
+################################################
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+print("min =", min(arr))
+print("max =", max(arr))
